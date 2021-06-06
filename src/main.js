5b0cfb11ebe55548ec9c758511a5a8a0340c3d0a
Gao Weihan <15940881805@163.com>
2021-06-06 23:25:36 +0800

    增加element-ui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import { ElButton, ElSelect } from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component(ElButton.name, ElButton)
+app.component(ElSelect.name, ElSelect)
+app.mount('#app')
---
+++ src/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main">123</div>
+    <el-button>测试</el-button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'index',
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      alert('登录')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  $bg: red;
+
+  .main {
+    width: 100px;
+    height: 100px;
+    background: $bg;
+  }
+</style>
